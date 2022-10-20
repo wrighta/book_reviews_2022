820486f09d878a84c9f68a39a6a9d35c897e4d61
Anne Wright <anne.wright@iadt.ie>
2022-10-20 15:06:35 +0100

    index() contains a few different examples on how you can retrieve all books

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Book;
 use App\Http\Requests\StoreBookRequest;
 use App\Http\Requests\UpdateBookRequest;
+use Illuminate\Support\Facades\Auth;
 
 class BookController extends Controller
 {
@@ -15,7 +16,18 @@ class BookController extends Controller
      */
     public function index()
     {
-        //
+          // Fetch All Books, owned by the Logged in User, in order of when they were last updated - latest updated returned first, And you want the $books paginated.
+          // $books = Book::where('user_id', Auth::id())->latest('updated_at')->paginate(10);
+
+          // Fetch All Books(not just belonging to the logged in user) and add pagination.
+          $books = Book::paginate(10);
+
+          // Fetch All Books, no pagination.
+          //$books = Book::all();
+
+          // Something not working - unccoment the line below to dump all the books onto the screen to help you troubleshoot.
+          //   dd($notes);
+          return view('books.index')->with('books', $books);
     }
 
     /**
