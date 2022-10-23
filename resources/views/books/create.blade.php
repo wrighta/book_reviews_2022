0754cdb310ab6c8b5fff6d3b31242002b39a39ab
Anne Wright <anne.wright@iadt.ie>
2022-10-23 20:38:23 +0100

    Fileupload functionalty added, including enctype multipart

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
-                <form action="{{ route('books.store') }}" method="post">
+                <form action="{{ route('books.store') }}" method="post" enctype="multipart/form-data">
                     @csrf
                     <x-text-input
                         type="text"
@@ -42,6 +42,14 @@
                         placeholder="Author..."
                         class="w-full mt-6"
                         :value="@old('author')"></x-text-input>
+
+                    <x-file-input
+                        type="file"
+                        name="book_image"
+                        placeholder="Book"
+                        class="w-full mt-6"
+                        field="book_image">
+                    </x-file-input>
 
                     <x-primary-button class="mt-6">Save Book</x-primary-button>
                 </form>
