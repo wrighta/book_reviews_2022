a7f0f4321711e09e9308f77fcf62a00ea82ebe93
Anne Wright <anne.wright@iadt.ie>
2022-11-20 15:38:37 +0000

    publisherIndex() determines if user is Admin or User and redirects them to the correct route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -39,4 +39,21 @@ class HomeController extends Controller
         }
         return redirect()->route($home);
     }
+
+    public function publisherIndex(Request $request)
+    {
+
+        $user = Auth::user();
+        $home = 'home';
+
+        if($user->hasRole('admin')){
+            $home = 'admin.publishers.index';
+        }
+        else if ($user->hasRole('user')){
+            $home = 'user.publishers.index';
+        }
+        return redirect()->route($home);
+    }
+
+
 }
