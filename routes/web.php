b8be5456520e4e687af0fedc5fb622ecfa40d457
Anne Wright <anne.wright@iadt.ie>
2022-11-20 15:41:52 +0000

    New routes for home/publishers, admin/publishers and user/publishers

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\Admin\BookController as AdminBookController;
 use App\Http\Controllers\User\BookController as UserBookController;
+use App\Http\Controllers\Admin\PublisherController as AdminPublisherController;
+use App\Http\Controllers\User\PublisherController as UserPublisherController;
+
 use Database\Seeders\BookSeeder;
 use Illuminate\Support\Facades\Route;
 
@@ -27,11 +30,19 @@ Route::get('/dashboard', function () {
 require __DIR__.'/auth.php';
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home.index');
+Route::get('/home/publishers', [App\Http\Controllers\HomeController::class, 'publisherIndex'])->name('home.publisher.index');
+
 
 // This will create all the routes for Book
 // and the routes will only be available when a user is logged in
 Route::resource('/admin/books', AdminBookController::class)->middleware(['auth'])->names('admin.books');
-
 Route::resource('/user/books', UserBookController::class)->middleware(['auth'])->names('user.books')->only(['index', 'show']);
 
+
+// This will create all the routes for Publisher functionality.
+// and the routes will only be available when a user is logged in
+Route::resource('/admin/publishers', AdminPublisherController::class)->middleware(['auth'])->names('admin.publishers');
+
+// the ->only at the end of this statement says only create the index and show routes.
+Route::resource('/user/publishers',UserPublisherController::class)->middleware(['auth'])->names('user.publishers')->only(['index', 'show']);
 
